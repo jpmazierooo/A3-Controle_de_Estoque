16298e8d8a5197749ec013197da77ebe50073bb2
João Pedro Maziero <Joo Pedro Maziero@DESKTOP-K8F9F7K>
2026-04-15 20:51:54 -0300

    feat: implementa classe Movimentacao (model)
    criação dos construtores padrão e parametrizado
    implementação dos getters e setters
    estrutura inicial da entidade Movimentacao pronta para uso

--- src/main/java/model/Movimentacao.java
+++ src/main/java/model/Movimentacao.java
@@ -1,13 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package model;
 
-/**
- *
- * @author João Pedro Maziero
- */
 public class Movimentacao {
+    private String nomeprod;
+    private String datamov;
+    private double qtdmov;
+    private String tipomov;
+
+    public Movimentacao() {
+    }
+
+    public Movimentacao(String nomeprod, String datamov, double qtdmov, String tipomov) {
+        this.nomeprod = nomeprod;
+        this.datamov = datamov;
+        this.qtdmov = qtdmov;
+        this.tipomov = tipomov;
+    }
+
+    public String getNomeprod() {
+        return nomeprod;
+    }
+
+    public void setNomeprod(String nomeprod) {
+        this.nomeprod = nomeprod;
+    }
+
+    public String getDatamov() {
+        return datamov;
+    }
+
+    public void setDatamov(String datamov) {
+        this.datamov = datamov;
+    }
+
+    public double getQtdmov() {
+        return qtdmov;
+    }
+
+    public void setQtdmov(double qtdmov) {
+        this.qtdmov = qtdmov;
+    }
+
+    public String getTipomov() {
+        return tipomov;
+    }
+
+    public void setTipomov(String tipomov) {
+        this.tipomov = tipomov;
+    }
     
 }
