6faf556fb8437704290ad5bf4bac90052d0b7431
JoaoPedroMarimon <marimonjoaopedro@gmail.com>
2026-05-07 20:33:57 -0300

    feat: adciona table categoria

--- banco.sql
+++ banco.sql
@@ -1,3 +1,10 @@
 CREATE DATABASE IF NOT EXISTS sistema_estoque;
 
 USE sistema_estoque;
+
+CREATE TABLE categoria (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    tamanho ENUM('PEQUENO', 'MEDIO', 'GRANDE') NOT NULL,
+    embalagem ENUM('LATA', 'VIDRO', 'PLASTICO') NOT NULL
+);
